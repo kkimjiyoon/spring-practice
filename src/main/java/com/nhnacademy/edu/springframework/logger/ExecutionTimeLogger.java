e36389755d4df18c405fed346ff76f825970dd6a
{kkimjiyoon} <{bystar1687@nate.com}>
2024-02-01 10:54:36 +0900

    commit

--- src/main/java/com/nhnacademy/edu/springframework/logger/ExecutionTimeLogger.java
+++ src/main/java/com/nhnacademy/edu/springframework/logger/ExecutionTimeLogger.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.edu.springframework.logger;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
 
 //@Aspect
@@ -9,6 +11,8 @@ public class ExecutionTimeLogger {
 
 //    @Around("execution(* com.nhnacademy.edu.springframework.messagesender.sender.MessageSender.sendMessage(..))")
     public Object printExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        Logger logger = LoggerFactory.getLogger(ExecutionTimeLogger.class);
+
         StopWatch stopWatch = new StopWatch("ExecutionLogger");
         stopWatch.start();
 
@@ -18,7 +22,12 @@ public class ExecutionTimeLogger {
             throw e;
         } finally {
             stopWatch.stop();
-            System.out.println(stopWatch.prettyPrint());
+//            System.out.println(stopWatch.prettyPrint());
+            String className = joinPoint.getTarget().getClass().getSimpleName();
+            String methodName = joinPoint.getSignature().getName();
+            String logMessage = String.format("%s.%s %s", className, methodName, stopWatch.getTotalTimeMillis() + "ms");
+
+            System.out.println(logMessage);
         }
     }
 

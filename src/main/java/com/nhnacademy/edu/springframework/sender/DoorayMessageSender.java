6089a72b1727d603ac0082c80b5e9601a33d5354
{kkimjiyoon} <{bystar1687@nate.com}>
2024-02-01 15:05:18 +0900

    commit

--- src/main/java/com/nhnacademy/edu/springframework/sender/DoorayMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/sender/DoorayMessageSender.java
@@ -18,11 +18,29 @@ public class DoorayMessageSender implements MessageSender {
     @Value("${comment}")
     private String comment;
 
+
+
+    /*@Autowired
+    private DoorayHookSender doorayHookSender; (필드주입 방식)*/
+
+
+    /*private DoorayHookSender doorayHookSender;
+    public DoorayMessageSender() {
+
+    }
+    @Autowired
+    public void setDoorayHookSender(DoorayHookSender doorayHookSender) {
+        this.doorayHookSender = doorayHookSender;
+    }(setter 주입 방식)*/
+
+
     private final DoorayHookSender doorayHookSender;
 
     public DoorayMessageSender(DoorayHookSender doorayHookSender) {
         this.doorayHookSender = doorayHookSender;
-    }
+    } // (생성자 주입 방식 Autowired 생략 가능)
+
+
     @Override
     @ExecutionTime
     public boolean sendMessage() {
